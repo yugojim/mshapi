896438346d2023ada27c97b4acb3523e381cdbe9
yugojim <yugojim@gmail.com>
2023-08-10 14:50:47 +0800

    fix consent bug

--- flaska.py
+++ flaska.py
@@ -148,7 +148,7 @@ def query_VisitNote():
                 Search = Search + 'date=le' + request.args.get('ltDate')           
             url = fhir + 'Composition/?' + Search + '&_count=365&_sort=-date'
             #+ 'title=門診'
-            #print(url)
+            print(url)
             response = requests.request("GET", url, headers={}, data={}, verify=False)
             resultjson=json.loads(response.text)
             return jsonify(resultjson), 200
